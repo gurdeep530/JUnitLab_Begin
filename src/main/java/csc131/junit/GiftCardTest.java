09d6f77b3f84ce829ca4f06321d29e7d3b278d7d
Gurdeep <gurdeep@Gurvirs-MacBook-Pro.local>
2021-03-13 16:00:41 -0800

    Adding test file

---
+++ src/main/java/csc131/junit/GiftCardTest.java
@@ -0,0 +1,99 @@
+package csc131.junit;
+
+import static org.junit.Assert.*;
+
+import org.junit.jupiter.api.Test;
+
+class GiftCardTest {
+
+	@Test
+	void testGetIssuingStore() {
+		
+		double       balance;
+		GiftCard     card; 
+		int 		issuingStore;
+		
+		issuingStore = 1337; 
+		balance      = 100.00; 
+		card = new GiftCard(issuingStore, balance);
+		
+		assertEquals("getIssuingStore()",  issuingStore, card.getIssuingStore()); 
+		
+	}
+	@Test
+	void getBalance() {
+		
+		double balance;
+		GiftCard card;
+		int issuingStore;
+		
+		issuingStore = 1337;
+		balance = 100.00;
+		card = new GiftCard(issuingStore, balance);
+		
+		assertEquals("getBalance()", balance, card.getBalance(), 0.001);
+	}
+	
+	@Test
+	void deduct_RemainingBalance()
+	{
+		double balance;
+		GiftCard card;
+		int issuingStore;
+		String s;
+		
+		issuingStore = 1337;
+		balance = 100.00;
+		card = new GiftCard(issuingStore, balance);
+		
+		s = "Remaining Balance: " + String.format("%6.2f", 90.00);
+		assertEquals("deduct(10.00)", s, card.deduct(10.0));
+		
+	}
+	
+	@Test 
+	void deduct_AmountDue() {
+		double balance;
+		GiftCard card;
+		int issuingStore;
+		String s;
+		
+		issuingStore = 1337;
+		balance = 100.00;
+		card = new GiftCard(issuingStore, balance);
+		
+		s = "Amount Due: " + String.format("%6.2f", 10.00);
+		assertEquals("deduct 110.00 form 100.00", s, card.deduct(110.0));
+		
+	}
+	
+	@Test
+	void deduct_InvalidTransaction(){
+		double balance;
+		GiftCard card;
+		int issuingStore;
+		String s;
+		
+		issuingStore = 1337;
+		balance = 100.00;
+		card = new GiftCard(issuingStore, balance);
+		
+		s = "Invalid Transaction: ";
+		assertEquals("deduct -10.00 from 100.00", s, card.deduct(-10.00));
+			
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	void constructor_IncorrectID_Low()
+	{
+		new GiftCard(-1,100.00);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	void constructor_IncorrectID_High()
+	{
+		new GiftCard(100000,100.00);
+	}
+
+
+}
